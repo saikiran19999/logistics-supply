083fef963172d2d6c89d35062011172f7c8a723c
saikiran19999 <50092036+saikiran19999@users.noreply.github.com>
2024-04-08 21:10:05 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,7 +5,7 @@ $username = "sai";
 $password = "sai";
 $db = "cms_db";
 
-$conn = new mysqli_connect($servername,$username,$password,$db);
+$conn = new mysqli($servername,$username,$password,$db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
